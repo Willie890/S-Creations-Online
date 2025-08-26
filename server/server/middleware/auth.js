76f8c5a87a2637cb4250dfa974889d1a72edad48
Willie890 <Willemstefanus56@gmail.com>
2025-08-26 18:50:10 +0200

    Update auth.js

--- server/server/middleware/auth.js
+++ server/server/middleware/auth.js
@@ -1,36 +1,62 @@
-// server/middleware/auth.js
-const jwt = require('jsonwebtoken')
-const User = require('../models/User')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const AppError = require('../utils/AppError');
 
 exports.protect = async (req, res, next) => {
-  let token
+  let token;
   
-  if (req.cookies.token) {
-    token = req.cookies.token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
-  
+
   if (!token) {
-    return res.status(401).json({
-      message: 'Not authorized to access this route'
-    })
+    return next(new AppError('Not authorized to access this route', 401));
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = await User.findById(decoded.id)
-    next()
-  } catch (err) {
-    return res.status(401).json({
-      message: 'Not authorized to access this route'
-    })
-  }
-}
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const currentUser = await User.findById(decoded.id);
+    
+    if (!currentUser) {
+      return next(new AppError('The user belonging to this token no longer exists', 401));
+    }
 
-exports.admin = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
-    return next()
+    req.user = currentUser;
+    next();
+  } catch (err) {
+    return next(new AppError('Not authorized to access this route', 401));
   }
-  return res.status(403).json({
-    message: 'Not authorized as admin'
-  })
-}
+};
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+    next();
+  };
+};
+
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.token) {
+    try {
+      const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+      const currentUser = await User.findById(decoded.id);
+      
+      if (currentUser) {
+        res.locals.user = currentUser;
+        return next();
+      }
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
+};
