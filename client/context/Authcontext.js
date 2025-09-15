14f3355dea915fa43670e8889f90cb10dcfdc979
Willie890 <Willemstefanus56@gmail.com>
2025-09-15 20:10:40 +0200

    Update Authcontext.js

--- client/context/Authcontext.js
+++ client/context/Authcontext.js
@@ -10,7 +10,7 @@ export function AuthProvider({ children }) {
   const router = useRouter();
 
   useEffect(() => {
-    axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+    axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1';
     axios.defaults.withCredentials = true;
   }, []);
 
