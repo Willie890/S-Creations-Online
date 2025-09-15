94133df2d2a83c0aaa2363b1aa0677eca9c9cb2c
Willie890 <Willemstefanus56@gmail.com>
2025-09-15 18:35:19 +0200

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   output: 'export',
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+    domains: ['res.cloudinary.com', 'localhost'],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1',
+  },
+  trailingSlash: true,
 }
