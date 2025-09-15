533593fee6955fce667f07a98c9c92adf6baa770
Willie890 <Willemstefanus56@gmail.com>
2025-09-15 18:38:28 +0200

    Update Layout.js

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,36 +1,19 @@
-import { useState } from 'react';
-import Head from 'next/head';
 import styled from 'styled-components';
 import { theme } from '../styles/theme';
 import Header from './Header';
 import Footer from './Footer';
-import Cart from './Cart';
 
 export default function Layout({ children }) {
-  const [cartOpen, setCartOpen] = useState(false);
-
   return (
-    <>
-      <Head>
-        <title>S-Creations Online</title>
-        <meta name="description" content="Premium handmade products" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-      <Container>
-        <Header onCartClick={() => setCartOpen(true)} />
-        <Main>
-          {children}
-        </Main>
-        <Footer />
-        
-        <Cart isOpen={cartOpen} onClose={() => setCartOpen(false)} />
-      </Container>
-    </>
+    <LayoutContainer>
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
+    </LayoutContainer>
   );
 }
 
-const Container = styled.div`
+const LayoutContainer = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
@@ -39,4 +22,5 @@ const Container = styled.div`
 const Main = styled.main`
   flex: 1;
   padding-top: 80px;
+  background-color: ${theme.colors.light};
 `;
