e2d2abbc674e63b3d1ca1ce5b59b7a583a1bbef2
Willie890 <Willemstefanus56@gmail.com>
2025-09-26 14:34:33 +0200

    Create _app.js

---
+++ client/pages/_app.js
@@ -0,0 +1,18 @@
+// client/pages/_app.js
+import { AuthProvider } from '../context/AuthContext';
+import { CartProvider } from '../context/CartContext';
+import GlobalStyles from '../styles/GlobalStyles';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles />
+      <AuthProvider>
+        <CartProvider>
+          <Component {...pageProps} />
+        </CartProvider>
+      </AuthProvider>
+    </>
+  );
+}
