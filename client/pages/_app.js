435f7b2787edd51de9ff242834866093ad7146f6
Willie890 <Willemstefanus56@gmail.com>
2025-09-26 16:48:48 +0200

    Update _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,20 +1,6 @@
 // client/pages/_app.js
-import '../styles/globals.css'; // ✅ Import CSS file
-import { ThemeProvider } from 'styled-components';
-
-// Optional: if you use styled-components global styles, create a separate file
-// import GlobalStyles from '../styles/StyledGlobalStyles';
-
-const theme = {
-  primary: '#4A6B3A',
-  secondary: '#D8B4E2',
-};
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ThemeProvider theme={theme}>
-      {/* {GlobalStyles && <GlobalStyles />} */}
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
