3767a795a3e777e630ba0f7bc28e97497afb4aa1
Willie890 <Willemstefanus56@gmail.com>
2025-11-03 18:34:47 +0200

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }
+}, { timestamps: true });
+
+// Pre-save hook to hash the password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
